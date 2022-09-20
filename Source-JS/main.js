32302446bd1829f6ae4a1ee53d3d90e89af1b349
bookmyteam <113586254+bookmyteam@users.noreply.github.com>
2022-09-20 10:38:52 +0700

    Update main.js

--- Source-JS/main.js
+++ Source-JS/main.js
@@ -1,3 +1,11 @@
+// Slider content
+var sliderContent = document.getElementById('slider-ct');
+setTimeout(() => {
+	sliderContent.style.transform = "translateX(70px)";
+	sliderContent.style.opacity = "1";
+}, 1000);
+// End Slider content
+
 // Slideshow
 var counter = 1;
 setInterval(() => {
