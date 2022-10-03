5df3879c3d57b4c9743fa5ee1ad018e57a0ae6db
bookmyteam <113586254+bookmyteam@users.noreply.github.com>
2022-10-03 11:07:30 +0700

    Add files via upload

---
+++ Source-JS/shop.js
@@ -0,0 +1,15 @@
+// Filter range
+var filter = document.getElementById('filter');
+var changePrice = document.getElementById('change-price');
+changePrice.innerHTML = filter.value
+
+filter.oninput = () => {
+    changePrice.innerHTML = filter.value;
+}
+// End Filter range
+
+// Get id book 
+    const getId = (clickedId) => {
+        localStorage.setItem("bId", clickedId);
+    }
+// End get id book
